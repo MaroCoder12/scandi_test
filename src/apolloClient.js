4304fb874a540bd16fc5f49e9070e1c0621ba3ba
MaroCoder12 <mrawanosama638@gmail.com>
2024-11-19 23:47:21 +0200

    ap1

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT || 'http://localhost:8000/index.php',
+  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT || 'https://scandtest.freesite.online/index.php',
   cache: new InMemoryCache(),
 });
 
