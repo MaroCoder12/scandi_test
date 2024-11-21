283e3f749b747cc45b77ffb63e0b8115c3b7ce0f
MaroCoder12 <mrawanosama638@gmail.com>
2024-11-21 03:10:19 +0200

    adding new host

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT || 'https://scandtest.freesite.online/index.php',
+  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT || 'https://oza.fc3.mytemp.website/index.php',
   cache: new InMemoryCache(),
 });
 
